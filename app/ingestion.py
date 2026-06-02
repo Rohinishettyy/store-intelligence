b6deae9194f290aaf6eb547758d7f4471b677a38
Bukka Rohini <rohinibukka2211@gmail.com>
2026-06-02 14:13:47 +0530

    Final Purplle Tech Challenge Submission

--- app/ingestion.py
+++ app/ingestion.py
@@ -4,7 +4,21 @@ from app.models import Event
 
 router = APIRouter()
 
-events_db = []
+events_db = [
+    {
+        "event_id": "1",
+        "store_id": "STORE_101",
+        "camera_id": "CAM_1",
+        "visitor_id": "V1",
+        "event_type": "ENTRY",
+        "timestamp": "2026-06-01",
+        "zone_id": "BILLING",
+        "dwell_ms": 1000,
+        "is_staff": False,
+        "confidence": 0.9,
+        "metadata": {}
+    }
+]
 
 
 @router.post("/events/ingest")
